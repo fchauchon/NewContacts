75edeac2f5313a51d705bc6920e4163bbec6d7c9
Frederic Chauchon <fchauchon@micropole.com>
2021-10-25 14:05:45 +0200

    Add 2 components and favicon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { Body2Component } from './body2/body2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: BodyComponent },
+    { path: 'body2', component: Body2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
