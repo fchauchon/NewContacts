d5c711396824a1f3f1de2f93d800dd82ee9d8100
Frederic Chauchon <fchauchon@micropole.com>
2021-11-19 17:27:44 +0100

    Day 3 with interceptor

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,17 +1,24 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
 
-    token = '0123456789';
+    token = null;
+
+    constructor(private dataService: DataService) {
+    }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
         if (req.url !== '/authen') {
-
+            if (this.token === null) {
+                this.token = this.dataService.getToken();
+            }
             const clone = req.clone({ setHeaders: { Authorization: `token ${this.token}` }});
             return next.handle(clone);
         }
