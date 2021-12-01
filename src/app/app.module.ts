557713979b837272360aa55b98202a27686e0635
Frederic Chauchon <fchauchon@micropole.com>
2021-12-01 13:24:15 +0100

    day: css fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,43 +13,43 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { InterceptorService } from './services/interceptor.service';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { FormComponent } from './form/form.component';
-import { ContactsListComponent } from './contacts-list/contacts-list.component';
-import { ContactComponent } from './contact/contact.component';
-import { TwoWaysComponent } from './two-ways/two-ways.component';
-import { ObsComponent } from './obs/obs.component';
-import { ProducerComponent } from './producer/producer.component';
 import { AboutComponent } from './about/about.component';
-import { SearchComponent } from './search/search.component';
-import { PasswordsComponent } from './passwords/passwords.component';
 import { AboutPageComponent } from './about-page/about.page.component';
-import { PasswordsPageComponent } from './passwords-page/passwords-page.component';
-import { ObsPageComponent } from './obs-page/obs-page.component';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { ContactPageComponent } from './contact-page/contact.page.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ObsComponent } from './obs/obs.component';
+import { ObsPageComponent } from './obs-page/obs-page.component';
+import { PasswordsComponent } from './passwords/passwords.component';
+import { PasswordsPageComponent } from './passwords-page/passwords-page.component';
+import { ProducerComponent } from './producer/producer.component';
+import { TwoWaysComponent } from './two-ways/two-ways.component';
 import { TwoWaysPageComponent } from './two-ways-page/two-ways-page.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
+    AboutComponent,
+    AboutPageComponent,
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ContactPageComponent,
-    FormComponent,
-    ContactsListComponent,
     ContactComponent,
-    TwoWaysPageComponent,
+    ContactPageComponent,
+    ContactsListComponent,
+    FormComponent,
+    FooterComponent,
+    HeaderComponent,
     TwoWaysComponent,
-    ObsPageComponent,
+    TwoWaysPageComponent,
     ObsComponent,
+    ObsPageComponent,
+    PasswordsComponent,
+    PasswordsPageComponent,
     ProducerComponent,
     SearchComponent,
-    PasswordsPageComponent,
-    PasswordsComponent,
-    AboutPageComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
